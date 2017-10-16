caac9b9ca3c8450305eed5396d64904613086d28
Gabriel Vargas <gabrielvcunha@gmail.com>
2017-10-16 10:02:20 -0300

    codigo de teste de disciplina

---
+++ modelos/disciplina.js
@@ -0,0 +1,39 @@
+"use strict";
+
+export default class Disciplina {
+    
+    constructor(_id, _nome, _codigo){
+        this.id = _id;
+        this.nome = _nome;
+        this.codigo = _codigo;
+    }
+
+    get id(){
+        return this.id + " teste id"; 
+    }
+
+    set id(_id){
+        if (typeof(_id) != "number") this.id = _id;
+    }
+
+    get getNome(){
+        return this.id + " teste nome"; 
+    }
+
+    set nome(_nome){
+        if (typeof(_nome) != "string") this.nome = _nome;
+    }
+
+    get codigo(){
+        return this.codigo + " teste codigo"; 
+    }
+
+    set codigo(_codigo){
+        if (typeof(_codigo) != "number") this.codigo = _codigo;
+    }
+
+    toString(){
+        console.log(this.id);
+        console.log(JSON.stringify(this));
+    }
+}
---
+++ test/disciplina_tst.js
@@ -0,0 +1,4 @@
+import Disciplina from "../modelos/disciplina";
+
+var disciplina = new Disciplina(1, "disciplinaTst", 123);
+console.log(disciplina);
