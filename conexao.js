be7edc5134b5f82e160a686ae698924a0708dfe6
Gabriel Vargas <gabrielvcunha@gmail.com>
2017-10-29 21:53:41 -0300

    Conetando ao banco azure

--- conexao.js
+++ conexao.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql';
 
 const conexao = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
+  host     : 'octoplus-mysqldbserver.mysql.database.azure.com',
+  user     : 'octo_sqlservice@octoplus-mysqldbserver',
+  password : 'FrameworkUFRRJ2017',
   database : 'db_frameworkufrrj'
 });
 
