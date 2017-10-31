914e34a79b51d82916863d6e3795386719364fa1
Karnack <Karnack@users.noreply.github.com>
2017-10-31 15:24:43 -0200

    Rotas. Controller
    Inserindo rotas e controller de material e conteudo. Não testado.

---
+++ DAO/conteudoDAO.js
@@ -0,0 +1,57 @@
+"use strict";
+import BD from '../BD.js';
+import Conteudo from '../modelos/Conteudo.js';
+
+export default class ConteudoDAO{
+
+    static async create(conteudo){
+        try {
+            var retorno = await BD.inserir(conteudo);
+            conteudo.setID(retorno);
+            return conteudo;
+        } catch (error) {
+            return error.message;
+        }
+
+    }
+
+    static async select(conteudoId){
+        try {
+            var conteudo = new Conteudo();
+            conteudo.setId(conteudoId);
+
+            var retorno = await BD.buscar(conteudo);
+              conteudo.setId(retorno.id);
+              conteudo.setNome(retorno.nome);
+              conteudo.setAula(retorno.aula);
+
+            return conteudo;
+
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    static async selectAll(){
+        return await BD.query("SELECT * FROM conteudo");
+   }
+
+
+    static async update(conteudo){
+      try{
+        return await BD.update(conteudo);
+
+      } catch (error) {
+         return error.message;
+      }
+    }
+
+    static async delete(conteudo){
+      try {
+          return await BD.deletar(conteudo);
+      } catch (error) {
+         return error.message;
+      }
+  }
+
+}
