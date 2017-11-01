8172d53a1c3b0c72d87787d87eb16b46cd18812f
Gabriel Vargas <gabrielvcunha@gmail.com>
2017-11-01 18:18:12 -0300

    Ajustando chamada de turmaRouter

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ rotas.use('/professor',professor)
 import periodo from './rotas/periodoRouter'
 rotas.use('/periodo',periodo)
 
-import turma from '.rotas/turmaRouter'
+import turma from './rotas/turmaRouter'
 rotas.use('/turma',turma)
+
 import alunos from './rotas/alunoRouter'
 rotas.use('/alunos',alunos)
 var crypto = require('crypto')
