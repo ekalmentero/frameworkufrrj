109db4e3906a60026b3848250deec98c79222b1e
Gabriel Vargas <gabrielvcunha@gmail.com>
2017-11-01 22:47:47 -0300

    Ajuste e uso do método parseEntidade em disciplinaDAO

--- modelos/entidade.js
+++ modelos/entidade.js
@@ -1,6 +1,7 @@
 export default class Entidade {
     parseEntidade(json){
         for (let property of Object.getOwnPropertyNames(json)){
+            if (property == "deleted") continue;
             eval("this.set" + property.charAt(0).toUpperCase() + property.slice(1) + "(json." + property + ")");
         }
     }
