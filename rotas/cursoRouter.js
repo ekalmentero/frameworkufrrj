7631f96848412b8fdbb7d74a33d0bbcbbef262a9
CaioFBittencourt <caioferreira.bittencourt@gmail.com>
2017-11-12 21:59:12 -0200

    adding departamento/cursos service

--- rotas/cursoRouter.js
+++ rotas/cursoRouter.js
@@ -2,23 +2,35 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import CursoController from '../controllers/cursoController';
 
-const curso = express.Router();
+const curso = express.Router({mergeParams: true});
 
 curso.use(bodyParser.json());
 
-curso.get('/:id', async function(req,res){
-    res.send(await CursoController.read(req.params.id));
+curso.get('/:id_curso', async function(req,res){
+  if (typeof(req.params.id_curso) != "undefined")
+      res.send(await CursoController.readByDepartamento(req.params.id_curso, req.params.id_departamento));
+  else
+      res.send(await CursoController.read(req.params.id_curso));
 })
 
 curso.route('/')
-  .post(async function(req,res){
-      res.send(await CursoController.create(req.body));
-  })
-  .patch(async function(req,res){
-      res.send(await CursoController.update(req.body));
-  })
-  .delete(async function(req,res){
-      res.send(await CursoController.delete(req.body));
+    .get(async function(req,res){
+        if (typeof(req.params.id_departamento) != "undefined")
+            res.send(await CursoController.readAllByDepartamento(req.params.id_departamento));
+        else
+            res.send(await CursoController.readAll());
+    })
+    .post(async function(req,res){
+        res.send(await CursoController.create(req.body));
+    })
+    .patch(async function(req,res){
+        if (typeof(req.params.id_departamento) != "undefined")
+            res.send(await CursoController.update(req.body, req.params.id_departamento));
+        else
+            res.send(await CursoController.update(req.body));
+    })
+    .delete(async function(req,res){
+        res.send(await CursoController.delete(req.body));
   });
 
 export default curso;
--- rotas/departamentoRouter.js
+++ rotas/departamentoRouter.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import disciplina from "./disciplinaRouter"
+import disciplinaRouter from "./disciplinaRouter";
+import cursoRouter from "./cursoRouter";
 import DepartamentoController from '../controllers/departamentoController';
+
 
 const departamento = express.Router();
 
 departamento.use(bodyParser.json());
 
-departamento.use('/:id_departamento/disciplinas',disciplina);
+departamento.use('/:id_departamento/disciplinas', disciplinaRouter);
+
+departamento.use('/:id_departamento/cursos', cursoRouter);
 
 departamento.get('/:id_departamento', async function(req,res){
     res.send(await DepartamentoController.read(req.params.id_departamento));
