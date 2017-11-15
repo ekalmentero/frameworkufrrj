ab010a9ee0b31ad76991a1a270aecb3ddfcf2947
Gabriel Vargas <gabrielvcunha@gmail.com>
2017-11-15 17:56:38 -0300

    [HOTFIX] Ajuste no método parseEntidade

--- modelos/entidade.js
+++ modelos/entidade.js
@@ -1,11 +1,12 @@
 export default class Entidade {
-    parseEntidade(json){
+    parseEntidade(json){ 
+        var properties = Object.getOwnPropertyNames(this);
         for (let property of Object.getOwnPropertyNames(json)){
             if (property == "deleted") continue;
+            if (!properties.includes(property)) continue;
             eval("this.set" + property.charAt(0).toUpperCase() + property.slice(1) + "(json." + property + ")");
         }
     }
-
 
     toString(){
         return JSON.stringify(this);
