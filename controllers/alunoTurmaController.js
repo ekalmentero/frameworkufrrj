9b599fe9c6427c9d0c598007f7dd4d8f60b11db4
CaioFBittencourt <caioferreira.bittencourt@gmail.com>
2017-11-20 18:51:52 -0200

    adding new files for nota_final service

---
+++ controllers/alunoTurmaController.js
@@ -0,0 +1,33 @@
+import AlunoTurma from '../modelos/alunoTurma';
+import AlunoTurmaDAO from '../DAO/AlunoTurmaDAO';
+export default class AlunoTurmaController {
+    static async create(alunoTurmaJson){
+        var alunoTurmaObj = new AlunoTurma();
+        alunoTurmaObj.parseEntidade(alunoTurmaJson);
+        return await AlunoTurmaDAO.create(alunoTurmaObj);
+    }
+
+    static async read(id){
+        var alunoTurmaObj = new AlunoTurma();
+        alunoTurmaObj.setId(id);
+        return await AlunoTurmaDAO.read(alunoTurmaObj);
+    }
+
+    static async readAll(){
+        return await AlunoTurmaDAO.readAll();
+    }
+
+    static async delete(alunoTurmaJson){
+        var alunoTurmaObj = new AlunoTurma();
+        alunoTurmaObj.parseEntidade(alunoTurmaJson);
+        return await AlunoTurmaDAO.delete(alunoTurmaObj);
+    }
+
+    static async update(alunoTurmaJson){
+        var alunoTurmaObj = new AlunoTurma();
+        alunoTurmaObj.parseEntidade(alunoTurmaJson);
+        return await AlunoTurmaDAO.update(alunoTurmaObj);
+    }
+
+
+}
---
+++ rotas/alunoTurmaRouter.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import AlunoTurmaController from '../controllers/alunoTurmaController';
+
+const alunoTurma = express.Router();
+
+alunoTurma.use(bodyParser.json());
+
+
+alunoTurma.route('/')
+  .post(async function(req,res){
+      res.send(await AlunoTurmaController.create(req.body, req.body.aluno, req.body.turma, req.body.conceito));
+  })
+  .get(async function(req,res){
+    res.send(await AlunoTurmaController.readAll());
+  })
+
+  .delete(async function(req,res){
+      res.send(await AlunoTurmaController.delete(req.body));
+  })
+  .patch(async function(req,res){
+      res.send(await AlunoTurmaController.update(req.body));
+  })
+
+export default alunoTurma;
