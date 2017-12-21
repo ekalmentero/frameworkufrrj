cded2b5ff3c101035475eb5bf810e17c61ce6cc6
Karnack <Karnack@users.noreply.github.com>
2017-12-21 11:38:01 -0200

    Update material
    adicionando queries e atualizando read.

--- DAO/materialDAO.js
+++ DAO/materialDAO.js
@@ -4,54 +4,49 @@ import Material from '../modelos/Material.js';
 
 export default class MaterialDAO{
 
-    static async create(material){
-        try {
-            var retorno = await BD.inserir(material);
-            material.setID(retorno);
-            return material;
-        } catch (error) {
-            return error.message;
-        }
+  static async create(material, id_conteudo){
+      try {
+        var foreignKeys = [];
+        foreignKeys.push(['conteudo', id_conteudo]);
+        var retorno = await BD.inserir(material, foreignKeys);
+        material.Id = retorno;
 
+        return material;
+
+      } catch (error) {
+        return error.message;
+      }
     }
 
-    static async select(materialId){
-        try {
-            var material = new Material();
-            material.setId(materialId);
-
-            var retorno = await BD.buscar(material);
-              material.setId(retorno.id);
-              material.setNome(retorno.nome);
-              material.setAula(retorno.aula);
-
-            return material;
-
-        } catch (error) {
-            return error.message;
-        }
+    static async read(material){
+      try {
+        var retorno = await BD.buscar(material);
+        return retorno[0];
+      } catch (error) {
+        return error.message;
+      }
     }
 
-    static async selectAll(){
+    static async readAll(){
         return await BD.query("SELECT * FROM material");
    }
+    
+   static async readByTurma(id_turma){
+     return await BD.query("SELECT DISTINCT m.arquivo FROM aula a INNER JOIN conteudo c ON a.id=c.aula INNER JOIN m.conteudo=c.id WHERE a.turma='"+id_turma+"'" );
+   }
 
-
-    static async update(material){
+    static async update(material, id_conteudo){
       try{
-        return await BD.update(material);
-
+        if (typeof(id_conteudo) == "undefined")
+          return await BD.update(material);
+        else{
+          var foreignKeys = [];
+          foreignKeys.push(['conteudo', id_conteudo]);
+          return await BD.update(material, foreignKeys);
+        }
       } catch (error) {
-         return error.message;
+        return error.message;
       }
     }
-
-    static async delete(material){
-      try {
-          return await BD.deletar(material);
-      } catch (error) {
-         return error.message;
-      }
-  }
 
 }
