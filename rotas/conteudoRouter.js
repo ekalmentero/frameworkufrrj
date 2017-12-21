f3b9c16960a01515df85727e0b25d6cc320fadd4
Karnack <Karnack@users.noreply.github.com>
2017-12-21 11:48:53 -0200

    Update conteudo rota

--- rotas/conteudoRouter.js
+++ rotas/conteudoRouter.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import ConteudoController from '../controllers/conteudoController';
 
-const conteudo = express.Router();
+const conteudo = express.Router({mergeParams: true});
 
 conteudo.use(bodyParser.json());
 
@@ -15,10 +15,13 @@ conteudo.route('/')
       res.send(await ConteudoController.readAll());
   })
   .post(async function(req,res){
-      res.send(await ConteudoController.create(req.body));
+       res.send(await conteudoController.create(req.body, req.params.id_aula));
   })
   .patch(async function(req,res){
-      res.send(await ConteudoController.update(req.body));
+    if (typeof(req.params.id_aula) != "undefined")
+        res.send(await conteudoController.update(req.body, req.params.id_aula));
+    else
+        res.send(await conteudoController.update(req.body));
   })
   .delete(async function(req,res){
       res.send(await ConteudoController.delete(req.body));
