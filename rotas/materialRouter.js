e1dd9c55f103cd961c67622affd72344681160ad
Karnack <Karnack@users.noreply.github.com>
2017-12-21 11:48:29 -0200

    updating material rota

--- rotas/materialRouter.js
+++ rotas/materialRouter.js
@@ -2,23 +2,32 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import MaterialController from '../controllers/materialController';
 
-const material = express.Router();
+const material = express.Router({mergeParams: true});
 
 material.use(bodyParser.json());
 
-material.get('/:id', async function(req,res){
-    res.send(await MaterialController.read(req.params.id));
+material.get('/:id_material', async function(req,res){
+    if (typeof(req.params.id_turma) != "undefined")
+        res.send(await materialController.readByTurma(req.params.id_material, req.params.id_turma));
+    else
+        res.send(await materialController.read(req.params.id_material));
 })
 
 material.route('/')
   .get(async function(req,res){
-      res.send(await MaterialController.readAll());
+    if (typeof(req.params.id_turma) != "undefined")
+        res.send(await materialController.readAllByTurma(req.params.id_turma));
+    else
+        res.send(await materialController.readAll());
   })
   .post(async function(req,res){
-      res.send(await MaterialController.create(req.body));
+      res.send(await MaterialController.create(req.body, req.params.id_conteudo));
   })
   .patch(async function(req,res){
-      res.send(await MaterialController.update(req.body));
+    if (typeof(req.params.id_conteudo) != "undefined")
+        res.send(await materialController.update(req.body, req.params.id_conteudo));
+    else
+        res.send(await materialController.update(req.body));
   })
   .delete(async function(req,res){
       res.send(await MaterialController.delete(req.body));
