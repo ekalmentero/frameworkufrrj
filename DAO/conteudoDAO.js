64afc9461cab47ed943a186067d5263c154531a2
Karnack <Karnack@users.noreply.github.com>
2017-12-21 11:40:10 -0200

    update conteudo
    atualizando conteudo

--- DAO/conteudoDAO.js
+++ DAO/conteudoDAO.js
@@ -4,47 +4,46 @@ import Conteudo from '../modelos/Conteudo.js';
 
 export default class ConteudoDAO{
 
-    static async create(conteudo){
+    static async create(conteudo, id_aula){
         try {
-            var retorno = await BD.inserir(conteudo);
-            conteudo.setID(retorno);
-            return conteudo;
-        } catch (error) {
-            return error.message;
-        }
+          var foreignKeys = [];
+          foreignKeys.push(['aula', id_aula]);
+          var retorno = await BD.inserir(conteudo, foreignKeys);
+          conteudo.Id = retorno;
 
-    }
-
-    static async select(conteudoId){
-        try {
-            var conteudo = new Conteudo();
-            conteudo.setId(conteudoId);
-
-            var retorno = await BD.buscar(conteudo);
-              conteudo.setId(retorno.id);
-              conteudo.setNome(retorno.nome);
-              conteudo.setAula(retorno.aula);
-
-            return conteudo;
+          return conteudo;
 
         } catch (error) {
-            return error.message;
+          return error.message;
         }
-    }
+      }
 
-    static async selectAll(){
+    static async read(conteudo){
+        try {
+          var retorno = await BD.buscar(conteudo);
+          return retorno[0];
+        } catch (error) {
+          return error.message;
+        }
+      }
+
+  static async readAll() {
         return await BD.query("SELECT * FROM conteudo");
    }
 
-
-    static async update(conteudo){
-      try{
-        return await BD.update(conteudo);
-
-      } catch (error) {
-         return error.message;
-      }
-    }
+   static async update(conteudo, id_aula) {
+     try {
+       if (typeof(id_aula) == "undefined")
+         return await BD.update(conteudo);
+       else{
+         var foreignKeys = [];
+         foreignKeys.push(['aula', id_aula]);
+         return await BD.update(conteudo, foreignKeys);
+       }
+     } catch (error) {
+       return error.message;
+     }
+   }
 
     static async delete(conteudo){
       try {
