58f153bc01bd20a806a98d1e3183541e34cdb507
Gabriel Vargas <gabrielvcunha@gmail.com>
2018-06-10 18:15:34 -0300

    Refactoring routes

--- router/index.js
+++ router/index.js
@@ -3,34 +3,34 @@ import express from 'express';
 const router = express.Router();
 
 router.all('/', function(req, res) {
-    res.send("TESTE");
+    res.send("UFRRJ API v0.1 by Curso de Sistemas de Informação UFRRJ (http://cursos.ufrrj.br/grad/sistemas/)");
 })
 
 import calendario from './calendarioRouter';
-router.use('/calendario',calendario);
+router.use('/calendarios',calendario);
 
 import disciplinas from './disciplinaRouter';
 router.use('/disciplinas',disciplinas);
 
 import curso from './cursoRouter';
-router.use('/curso', curso);
+router.use('/cursos', curso);
 
 import departamento from './departamentoRouter';
-router.use('/departamento', departamento);
+router.use('/departamentos', departamento);
 
 import professor from './professorRouter';
-router.use('/professor',professor);
+router.use('/professores',professor);
 
 import periodo from './periodoRouter';
-router.use('/periodo',periodo);
+router.use('/periodos',periodo);
 
 import turma from './turmaRouter';
-router.use('/turma',turma);
+router.use('/turmas',turma);
 
 import alunos from './alunoRouter';
 router.use('/alunos',alunos);
 
 import avaliacao from './avaliacaoRouter';
-router.use('/avaliacao', avaliacao);
+router.use('/avaliacoes', avaliacao);
 
 export default router;
